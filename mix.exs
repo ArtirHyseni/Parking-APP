8607862b52767ae05df6a882cb22693a1f9d3739
Samuel Roberti <samuel.p.roberti@gmail.com>
2019-10-14 04:35:55 -0400

    Add new dependencies for password encryption.

--- mix.exs
+++ mix.exs
@@ -37,6 +37,8 @@ defmodule Parker.MixProject do
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.1"},
+      {:comeonin, "~> 4.0"},
+      {:bcrypt_elixir, "~> 1.0"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
