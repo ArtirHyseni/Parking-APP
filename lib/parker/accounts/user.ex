257f46e560bdda619b32f75586958f7b7bdbb691
Samuel Roberti <samuel.p.roberti@gmail.com>
2019-10-18 07:06:00 -0400

    Add data validation pipeline to User schema.

--- lib/parker/accounts/user.ex
+++ lib/parker/accounts/user.ex
@@ -1,12 +1,16 @@
 defmodule Parker.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Parker.Accounts.{User, Encryption}
 
   schema "users" do
     field :password_hash, :string
     field :username, :string
+    field :email_hash, :string
 
     #Virtual fields
+    field :email, :string, virtual: true
+    field :email_confirmation, :string, virtual: true
     field :password, :string, virtual: true
     field :password_confirmation, :string, virtual: true
 
@@ -16,7 +20,39 @@ defmodule Parker.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:username, :password_hash])
-    |> validate_required([:username, :password_hash])
+    |> cast(attrs, [:username, :password, :email])
+    |> validate_required([:username])
+    |> validate_required([:password])
+    |> validate_required([:email])
+    |> validate_length(:password, min: 8)
+    |> validate_length(:username, min: 6)
+    |> validate_confirmation(:password)
+    |> validate_confirmation(:email)
+    |> unique_constraint(:username)
+    |> downcase
+    |> encrypt_login
+    |> unique_constraint(:email_hash)
   end
+
+  defp downcase(changeset) do
+    update_change(changeset, :username, &String.downcase/1)
+    |> update_change(:username, &String.downcase/1)
+  end
+
+
+  defp encrypt_login(changeset) do
+    changeset
+    |> encrypt_field(:password, :password_hash)
+    |> encrypt_field(:email, :email_hash)
+  end
+
+  defp encrypt_field(changeset, field, field_hash) do
+    get_change(changeset, field)
+    |> Encryption.hash_field
+    |> (&(put_change(changeset, field_hash, &1))).()
+  end
+
 end
+
+
+
