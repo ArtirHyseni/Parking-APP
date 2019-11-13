bdc01152a688e2fed1a7ff055f6ecc23fe5958dd
Samuel Roberti <samuel.p.roberti@gmail.com>
2019-11-12 21:58:55 -0500

    Adjust User path in router to match generated tests.

--- lib/parker_web/router.ex
+++ lib/parker_web/router.ex
@@ -23,7 +23,8 @@ defmodule ParkerWeb.Router do
   scope "/api", ParkerWeb do
     pipe_through :api
 
-    resources "/users", UserController, only: [:create, :new]
+#    resources "/users", UserController, only: [:create, :new]
+    post "/users/", UserController, :create
     get "/login", SessionController, :new
     post "/login", SessionController, :login
     delete "/logout", SessionController, :logout
